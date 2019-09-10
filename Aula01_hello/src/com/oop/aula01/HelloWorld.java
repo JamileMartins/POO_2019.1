fbdcea6b2b0f925c57c754dc240e4a9249935f5c
Jamile Martins <p.jamile.eti@gmail.com>
2019-09-10 18:27:19 -0300

    Aula 01 - Hello, World
    Primeiro programa em Java no Eclipse.

---
+++ Aula01_hello/src/com/oop/aula01/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.oop.aula01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		System.out.println("\nPrincipais comandos no Eclipse:");
+		System.out.println("\tIdentar o código: Ctrl + I");
+		System.out.println("\tFormatar o código: Ctrl + Shift + F");
+		System.out.println("\tAumentar o Zoom: Ctrl + =");
+		System.out.println("\tDiminuir o Zoom: Ctrl + -");
+
+	}
+
+}
